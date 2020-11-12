8dc6ca2c2a62839ca5dcc56b7a9611b76c38277b
Dominic Scheirlinck <dominic@vitaler.com>
2020-11-12 15:33:19 +1300

    feat: configure ttl on table once it is ready

--- src/cmd/install.ts
+++ src/cmd/install.ts
@@ -1,4 +1,5 @@
 import { AWSError } from 'aws-sdk';
+import { DescribeTableOutput } from 'aws-sdk/clients/dynamodb';
 import debug from 'debug';
 import { CommandModule } from 'yargs';
 import { CACHE_METADATA_TABLE_DEFINITION, CACHE_METADATA_TABLE_NAME } from '../cache-metadata';
@@ -6,7 +7,53 @@ import { service } from '../dynamodb';
 
 const log = debug('monofo:cmd:install');
 
+function delay(millis: number): Promise<void> {
+  return new Promise((next) => {
+    setTimeout(() => {
+      next();
+    }, millis);
+  });
+}
+
+async function describeTable(): Promise<DescribeTableOutput | undefined> {
+  let result;
+
+  try {
+    result = await service.describeTable({ TableName: CACHE_METADATA_TABLE_NAME }).promise();
+  } catch (e) {
+    if ((e as AWSError).code === 'ResourceNotFoundException') {
+      return undefined;
+    }
+
+    throw e;
+  }
+
+  return result;
+}
+
+async function tableReady(): Promise<void> {
+  let i = 0;
+  do {
+    log('Waiting for table to be ready');
+
+    // eslint-disable-next-line no-await-in-loop
+    const description = await describeTable();
+
+    if (description?.Table?.TableStatus === 'ACTIVE') {
+      log('Table is ready');
+      return;
+    }
+
+    // eslint-disable-next-line no-await-in-loop
+    await delay(2000);
+    i += 1;
+  } while (i < 10);
+
+  log('Gave up waiting for table ready');
+}
+
 async function createTable(): Promise<void> {
+  log('Creating table');
   try {
     await service.createTable(CACHE_METADATA_TABLE_DEFINITION).promise();
   } catch (e) {
@@ -17,19 +64,19 @@ async function createTable(): Promise<void> {
 
     throw e;
   }
-}
 
-async function tableExists(): Promise<boolean> {
-  try {
-    await service.describeTable({ TableName: CACHE_METADATA_TABLE_NAME }).promise();
-    return true;
-  } catch (e) {
-    if ((e as AWSError).code === 'ResourceNotFoundException') {
-      return false;
-    }
+  await tableReady();
 
-    throw e;
-  }
+  log('Configuring TTL');
+  await service
+    .updateTimeToLive({
+      TableName: CACHE_METADATA_TABLE_NAME,
+      TimeToLiveSpecification: {
+        Enabled: true,
+        AttributeName: 'expiresAt',
+      },
+    })
+    .promise();
 }
 
 const cmd: CommandModule = {
@@ -39,7 +86,7 @@ const cmd: CommandModule = {
   builder: {},
 
   async handler(): Promise<void> {
-    if (!(await tableExists())) {
+    if (!(await describeTable())) {
       await createTable();
       log('Table was installed');
     } else {
