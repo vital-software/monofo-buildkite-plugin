7c67ffd21154a70e888352610b57738e615aafd6
Dominic Scheirlinck <dominic@vitaler.com>
2020-11-12 14:43:36 +1300

    feat: support multiple builds in the artifact injection step

--- src/steps/artifact-injection.ts
+++ src/steps/artifact-injection.ts
@@ -1,4 +1,5 @@
 import debug from 'debug';
+import _ from 'lodash';
 import Config from '../config';
 
 const log = debug('monofo:steps:artifact-injection');
@@ -7,32 +8,41 @@ export const ARTIFACT_INJECTION_STEP_KEY = 'monorepo-inject-artifacts';
 const ARTIFACT_INJECTION_STEP_LABEL = `:crystal_ball:`;
 
 /**
- * @todo Should group into parallel steps by build ID?
+ * @todo One step, multiple plugin configs
  */
 export function artifactInjectionSteps(configs: Config[]): Step[] {
-  const names = configs.filter((c) => !c.included).map((c) => c.monorepo.name);
-  const produces = configs
-    .filter((c) => !c.included)
-    .flatMap((e) => e.monorepo.produces)
-    .filter((artifact) => !artifact.startsWith('.phony/'));
-  const { buildId } = configs[0];
+  const skipped = _(configs).filter((c) => !c.included);
+  const names = skipped.map((c) => c.monorepo.name).value();
 
-  if (names.length < 1 || produces.length < 1) {
+  const allProducedArtifacts = skipped
+    .flatMap((e) => e.monorepo.produces)
+    .filter((artifact) => !artifact.startsWith('.phony/'))
+    .value();
+
+  if (allProducedArtifacts.length < 1) {
     log("Not adding inject artifacts step: doesn't seem to be needed by any steps");
     return [];
   }
 
-  if (!buildId) {
-    log('Not adding inject artifacts step: no build ID found');
-    return [];
-  }
+  const plugins: ArtifactPluginConfig[] = skipped
+    .groupBy((c) => c.buildId)
+    .flatMap((configsForBuild) => {
+      const { buildId } = configsForBuild[0];
 
-  return [
-    {
-      key: ARTIFACT_INJECTION_STEP_KEY,
-      label: ARTIFACT_INJECTION_STEP_LABEL,
-      command: `echo 'inject for: ${names.join(', ')}'`,
-      plugins: [
+      if (!buildId) {
+        log('Not adding inject artifacts step: no build ID found');
+        return [];
+      }
+
+      const produces = configsForBuild
+        .flatMap((e) => e.monorepo.produces)
+        .filter((artifact) => !artifact.startsWith('.phony/'));
+
+      if (produces.length < 1) {
+        return [];
+      }
+
+      return [
         {
           'artifacts#v1.3.0': {
             build: buildId,
@@ -40,7 +50,23 @@ export function artifactInjectionSteps(configs: Config[]): Step[] {
             upload: produces,
           },
         },
-      ],
+      ];
+    })
+    .value();
+
+  const step: CommandStep = {
+    key: ARTIFACT_INJECTION_STEP_KEY,
+    label: ARTIFACT_INJECTION_STEP_LABEL,
+    command: `echo 'inject for: ${names.join(', ')}'`,
+    plugins,
+  };
+
+  return [
+    {
+      key: ARTIFACT_INJECTION_STEP_KEY,
+      label: ARTIFACT_INJECTION_STEP_LABEL,
+      command: `echo 'inject for: ${names.join(', ')}'`,
+      plugins,
     } as CommandStep,
   ];
 }
