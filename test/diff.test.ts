4755c34c7c5b59fd15614040ae073696c4b8efc7
Dominic Scheirlinck <dominic@vitaler.com>
2021-06-22 13:06:32 +1200

    test: unit test of other diff algorithms

--- test/diff.test.ts
+++ test/diff.test.ts
@@ -3,7 +3,7 @@ import { mocked } from 'ts-jest/utils';
 import { getBuildkiteInfo } from '../src/buildkite/config';
 import Config from '../src/config';
 import { getBaseBuild, matchConfigs } from '../src/diff';
-import { mergeBase, revList } from '../src/git';
+import { mergeBase, revList, revParse } from '../src/git';
 import { COMMIT, fakeProcess } from './fixtures';
 
 jest.mock('../src/git');
@@ -14,14 +14,56 @@ mockRevList.mockImplementation(() => Promise.resolve([COMMIT]));
 
 const mockMergeBase = mocked(mergeBase, true);
 
-describe('getBaseBuild', () => {
-  it('returns the merge base on a non-default branch', async () => {
-    process.env = fakeProcess();
-    process.env.BUILDKITE_BRANCH = 'foo';
-    mockMergeBase.mockImplementation(() => Promise.resolve('foo'));
+const mockRevParse = mocked(revParse, true);
+mockRevParse.mockImplementation((foo) => Promise.resolve(foo));
 
-    const build = await getBaseBuild(getBuildkiteInfo());
-    expect(build.commit).toBe(COMMIT);
+describe('getBaseBuild', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('default branches', () => {
+    it('returns a previous build on a default branch', async () => {
+      process.env = fakeProcess();
+      process.env.BUILDKITE_BRANCH = 'foo';
+      process.env.BUILDKITE_PIPELINE_DEFAULT_BRANCH = 'foo';
+
+      const build = await getBaseBuild(getBuildkiteInfo());
+      expect(build.commit).toBe(COMMIT);
+    });
+
+    it('accepts MONOFO_DEFAULT_BRANCH to set the default branch', async () => {
+      process.env = fakeProcess();
+      process.env.BUILDKITE_BRANCH = 'foo';
+      process.env.BUILDKITE_PIPELINE_DEFAULT_BRANCH = 'bar';
+      process.env.MONOFO_DEFAULT_BRANCH = 'foo';
+
+      const build = await getBaseBuild(getBuildkiteInfo());
+      expect(build.commit).toBe(COMMIT);
+    });
+  });
+
+  describe('feature branches', () => {
+    it('returns the merge base on a feature branch', async () => {
+      process.env = fakeProcess();
+      process.env.BUILDKITE_BRANCH = 'foo';
+      mockMergeBase.mockImplementation(() => Promise.resolve('foo'));
+
+      const build = await getBaseBuild(getBuildkiteInfo());
+      expect(build.commit).toBe(COMMIT);
+    });
+  });
+
+  describe('integration branches', () => {
+    it('returns the merge base on a integration branch', async () => {
+      process.env = fakeProcess();
+      process.env.BUILDKITE_BRANCH = 'foo';
+      process.env.MONOFO_INTEGRATION_BRANCH = 'foo';
+      mockMergeBase.mockImplementation(() => Promise.resolve('foo'));
+
+      const build = await getBaseBuild(getBuildkiteInfo());
+      expect(build.commit).toBe(COMMIT);
+    });
   });
 });
 
