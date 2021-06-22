ead3de6e9ebad7603bd6296f2aa02fa9d5462604
Dominic Scheirlinck <dominic@vitaler.com>
2021-06-22 12:19:48 +1200

    feat: implement more lenient approach for integration branches
    Only considers previous builds, doesn't required them to match from
    history of current branch (because integration branches get reset)

--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,18 @@ export const plurals = (n: number): string => (n === 1 ? '' : 's');
 export const count = (arr: Array<unknown>, name: string): string => `${arr.length} ${name}${plurals(arr.length)}`;
 
 export type Command<T extends BaseArgs = BaseArgs> = CommandModule<T, T>;
+
+export function mapAsync<I, O>(
+  array: I[],
+  callback: (value: I, index: number, array: I[]) => Promise<O>
+): Promise<O[]> {
+  return Promise.all(array.map(callback));
+}
+
+export async function filterAsync<T>(
+  array: T[],
+  callback: (value: T, index: number, array: T[]) => Promise<boolean>
+): Promise<T[]> {
+  const filterMap = await mapAsync(array, callback);
+  return array.filter((_, index) => filterMap[index]);
+}
