c095f39563e3db38e8edb6b656c611f58228e9ac
Dominic Scheirlinck <dominic@vitaler.com>
2021-11-23 16:40:21 +1300

    feat(config-file): Allow customizing PIPELINE_FILE_GLOB
    and PIPELINE_FILE_IGNORE_GLOB

--- src/config-file.ts
+++ src/config-file.ts
@@ -23,11 +23,27 @@ function strings(v: undefined | string[] | string): string[] {
 export default class ConfigFile {
   private static readonly PIPELINE_FILE_REGEX = /^pipeline\.(?<name>.*)\.yml$/;
 
-  private static readonly PIPELINE_FILE_GLOB = '**/pipeline*.yml';
+  private static readonly PIPELINE_FILE_GLOB_DEFAULT = '**/pipeline*.yml';
 
-  private static readonly PIPELINE_FILE_IGNORE_GLOB = ['**/node_modules/**'];
+  private static readonly PIPELINE_FILE_IGNORE_GLOB_DEFAULT = ['**/node_modules/**'];
 
   constructor(public readonly path: string, public readonly basePath = process.cwd()) {}
+
+  private static fileGlob(): string {
+    if (process.env.PIPELINE_FILE_GLOB) {
+      return process.env.PIPELINE_FILE_GLOB;
+    }
+
+    return ConfigFile.PIPELINE_FILE_GLOB_DEFAULT;
+  }
+
+  private static fileIgnoreGlob(): string[] {
+    if (process.env.PIPELINE_FILE_IGNORE_GLOB) {
+      return process.env.PIPELINE_FILE_IGNORE_GLOB.split(':');
+    }
+
+    return ConfigFile.PIPELINE_FILE_IGNORE_GLOB_DEFAULT;
+  }
 
   nameFromFilename(): string | undefined {
     const match = ConfigFile.PIPELINE_FILE_REGEX.exec(basename(this.path));
@@ -39,10 +55,10 @@ export default class ConfigFile {
    */
   static async search(cwd: string = process.cwd()): Promise<ConfigFile[]> {
     try {
-      const pipelines = await glob(ConfigFile.PIPELINE_FILE_GLOB, {
+      const pipelines = await glob(ConfigFile.fileGlob(), {
         dot: true,
         cwd,
-        ignore: ConfigFile.PIPELINE_FILE_IGNORE_GLOB,
+        ignore: ConfigFile.fileIgnoreGlob(),
       });
 
       return pipelines.map((path) => {
--- test/config.test.ts
+++ test/config.test.ts
@@ -1,6 +1,7 @@
 import fsCb from 'fs';
 import path from 'path';
 import Config from '../src/config';
+import { fakeProcess } from './fixtures';
 
 const fs = fsCb.promises;
 
@@ -38,5 +39,29 @@ describe('getConfig()', () => {
     const configNames = (await Config.getAll(path.resolve(__dirname, 'projects/invalid'))).map((c) => c.monorepo.name);
     expect(configNames).toHaveLength(1);
     expect(configNames).toStrictEqual(['invalid']);
+  });
+
+  it('reads pipeline files - custom glob', async () => {
+    process.env = fakeProcess({
+      PIPELINE_FILE_GLOB: '**/pipeline.foo*.yml', // excludes foo1
+    });
+
+    const configNames = (await Config.getAll(path.resolve(__dirname, 'projects/flexible-structure'))).map(
+      (c) => c.monorepo.name
+    );
+    expect(configNames).toHaveLength(2);
+    expect(configNames).toStrictEqual(['foo2', 'foo3']);
+  });
+
+  it('reads pipeline files - custom ignore glob', async () => {
+    process.env = fakeProcess({
+      PIPELINE_FILE_IGNORE_GLOB: '**/node_modules/**:foo/**', // excludes foo2
+    });
+
+    const configNames = (await Config.getAll(path.resolve(__dirname, 'projects/flexible-structure'))).map(
+      (c) => c.monorepo.name
+    );
+    expect(configNames).toHaveLength(2);
+    expect(configNames).toStrictEqual(['foo1', 'foo3']);
   });
 });
