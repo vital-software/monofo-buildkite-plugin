cf596bb7c962d8519398174240b7667d272bee70
Dominic Scheirlinck <dominic@vitaler.com>
2021-12-08 00:46:03 +1300

    test: fix depdendency on DEBUG being turned on

--- test/artifacts/compression.test.ts
+++ test/artifacts/compression.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import { promisify } from 'util';
 import rimrafSync from 'rimraf';
 import tempy from 'tempy';
-import { inflator, deflator } from '../../src/artifacts/compression';
+import { inflator } from '../../src/artifacts/compression';
 import { Artifact } from '../../src/artifacts/model';
 import { fakeProcess } from '../fixtures';
 
@@ -14,7 +14,6 @@ describe('compression', () => {
   beforeAll(() => {
     dir = tempy.directory();
     process.chdir(dir);
-    console.log(`Using ${dir} for tests`);
 
     process.env = fakeProcess();
   });
@@ -25,10 +24,11 @@ describe('compression', () => {
 
   describe('inflator', () => {
     it('can download and inflate .tar.lz4 files correctly', async () => {
-      console.log(process.cwd());
       const artifact = new Artifact('foo.tar.lz4');
+
       const res = await inflator(fs.createReadStream(`${__dirname}/../fixtures/foo.tar.lz4`), artifact);
       expect(res.exitCode).toBe(0);
+
       expect(fs.existsSync(`${dir}`)).toBe(true);
       expect(fs.existsSync(`${dir}/foo/bar`)).toBe(true);
     });
