ccb9bf6f7456614f3ef0a72b68e9c80e0ae36aa4
Dominic Scheirlinck <dominic@vitaler.com>
2021-12-15 13:45:16 +1300

    fix: use --quiet flag to npx too

--- hooks/lib/run.bash
+++ hooks/lib/run.bash
@@ -8,5 +8,5 @@ export MONOFO_VERSION=${MONOFO_VERSION:-3.2.2}
 export DEBUG="monofo:*"
 
 function monofo() {
-    echo "npx -s sh monofo@$MONOFO_VERSION ${*}"
+    echo "npx --quiet --shell sh monofo@$MONOFO_VERSION ${*}"
 }
