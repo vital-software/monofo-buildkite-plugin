88524bca51ac5202f04ec6d01a6fd18b0ef7e692
Dominic Scheirlinck <dominic@vitaler.com>
2022-01-11 12:45:44 +1300

    fix: support single-string version of download property

--- hooks/lib/download.bash
+++ hooks/lib/download.bash
@@ -16,13 +16,14 @@ fi
 # The only format supported is a simple list of artifact filenames
 #
 # Example: BUILDKITE_PLUGIN_CONFIGURATION='{"download":["build.caidx","node-modules.tar.lz4"]}'
+# Example: BUILDKITE_PLUGIN_CONFIGURATION='{"download":"node-modules.tar.lz4"}'
 
 if [[ "${MONOFO_HOOK_DEBUG:-0}" -eq 1 ]]; then
   set -x
 fi
 
 flags=(download)
-files=$(echo "$BUILDKITE_PLUGIN_CONFIGURATION" | jq -rc '.download | .[]')
+files=$(echo "$BUILDKITE_PLUGIN_CONFIGURATION" | jq -rc '[.download] | flatten | .[]')
 
 for file in $files; do
   flags+=("$file")
