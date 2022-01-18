090695492f8bae5f8ca850820eee1ae89add5295
Dominic Scheirlinck <dominic@vitaler.com>
2022-01-18 14:49:46 +1300

    fix: calculate artifact name without pathname part

--- test/artifacts/model.test.ts
+++ test/artifacts/model.test.ts
@@ -2,17 +2,27 @@ import { Artifact } from '../../src/artifacts/model';
 import { BUILD_ID, BUILD_ID_2 } from '../fixtures';
 
 describe('model Artifact', () => {
-  it('looks up the right env vars to find the build', () => {
+  it('looks up the right env vars to find overridden build IDs', () => {
     process.env.BUILDKITE_BUILD_ID = BUILD_ID;
     process.env.MONOFO_ARTIFACT_NODE_MODULES_BUILD_ID = BUILD_ID_2;
 
-    const artifact = new Artifact('node-modules.tar.gz');
-    expect(artifact.buildId).toBe(BUILD_ID_2);
+    const hasExtraEnvVar = new Artifact('node-modules.tar.gz');
+    expect(hasExtraEnvVar.buildId).toBe(BUILD_ID_2);
 
-    const artifact2 = new Artifact('node_modules.tar.gz');
-    expect(artifact2.buildId).toBe(BUILD_ID_2);
+    const noExtraEnvVar = new Artifact('other.tar.gz');
+    expect(noExtraEnvVar.buildId).toBe(BUILD_ID);
+  });
 
-    const artifact3 = new Artifact('other.tar.gz');
-    expect(artifact3.buildId).toBe(BUILD_ID);
+  it.each([
+    ['node-modules.tar.gz', 'node-modules', 'tar.gz'],
+    ['/some/path/node-modules.tar.gz', 'node-modules', 'tar.gz'],
+    ['./node-modules.caidx', 'node-modules', 'caidx'],
+  ])('when given file %s produces name %s and ext %s', (filename, name, ext) => {
+    process.env.BUILDKITE_BUILD_ID = BUILD_ID;
+    process.env.MONOFO_ARTIFACT_NODE_MODULES_BUILD_ID = BUILD_ID_2;
+
+    const artifact = new Artifact(filename);
+    expect(artifact.name).toBe(name);
+    expect(artifact.ext).toBe(ext);
   });
 });
