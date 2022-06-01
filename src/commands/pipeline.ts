vital-software/monofo-buildkite-plugin

commit c1b2cec62c7ea46a9525d0fd53a78822f72be575

refactor: rewrite the pipeline command using async/await

src/commands/pipeline.ts
@@ -14,27 +14,27 @@ export default class Pipeline extends BaseCommand {
 
   static override flags = { ...BaseCommand.flags };
 
-  run() {
-    return Config.getAll(process.cwd())
-      .then((c) =>
-        c.length > 0 ? c : Promise.reject(new Error(`No pipeline files to process (cwd: ${process.cwd()})`))
-      )
-      .then((configs) => {
-        return getBaseBuild(getBuildkiteInfo(process.env))
-          .then((baseBuild) =>
-            diff(baseBuild.commit).then((changedFiles) => matchConfigs(baseBuild, configs, changedFiles))
-          )
-          .catch((e: unknown) => {
-            log('Failed to find base commit or diff changes, falling back to do a full build', e);
-            Config.configureFallback(configs);
-            return Promise.resolve();
-          })
-          .then(() => mergePipelines(configs));
-      })
-      .then(dumpYaml)
-      .then((v) => {
-        process.stdout.write(`${v}\n`);
-        return v;
-      });
+  async run() {
+    const configs = await Config.getAll(process.cwd());
+
+    if (configs.length < 1) {
+      throw new Error(`No pipeline files to process (cwd: ${process.cwd()})`);
+    }
+
+    try {
+      const baseBuild = await getBaseBuild(getBuildkiteInfo(process.env));
+      const changedFiles = await diff(baseBuild.commit);
+
+      matchConfigs(baseBuild, configs, changedFiles);
+    } catch (err: unknown) {
+      log('Failed to find base commit or diff changes, falling back to do a full build', err);
+      Config.configureFallback(configs);
+      return Promise.resolve();
+    }
+
+    const yaml = dumpYaml(await mergePipelines(configs));
+
+    process.stdout.write(`${yaml}\n`);
+    return yaml;
   }
 }

src/config.ts
@@ -105,12 +105,12 @@ export default class Config {
   }
 
   /**
-   * Iterator that yields every step in the config, including sub-steps attached to group steps
+   * Every step in the config, including sub-steps attached to group steps
    *
    * Useful for e.g. filtering
    */
-  public *allSteps(): IterableIterator<Step> {
-    yield* this.steps.flatMap((step) => {
+  public allSteps(): Step[] {
+    return this.steps.flatMap((step) => {
       if (isGroupStep(step)) {
         return [...step.steps, step];
       }
@@ -118,8 +118,8 @@ export default class Config {
     });
   }
 
-  public *outerSteps(): IterableIterator<Step> {
-    yield* this.steps;
+  public outerSteps(): Step[] {
+    return this.steps;
   }
 
   public useFallback(): void {

src/merge.ts
@@ -135,7 +135,9 @@ export default async function mergePipelines(configs: Config[]): Promise<Pipelin
   await sendBuildkiteAnnotation(configs);
 
   const artifactSteps = artifactInjectionSteps(configs);
+
   replaceExcludedKeys(configs, artifactSteps.length > 0);
+
   mergeGroups(configs);
 
   const pipelineParts: Pipeline[] = [
@@ -145,7 +147,9 @@ export default async function mergePipelines(configs: Config[]): Promise<Pipelin
     toPipeline(await recordSuccessSteps(configs)),
   ];
 
-  return _.mergeWith({}, ...pipelineParts, (dst: unknown, src: unknown) =>
+  const merged: Pipeline = _.mergeWith({}, ...pipelineParts, (dst: unknown, src: unknown) =>
     _.isArray(dst) ? dst.concat(src) : undefined
   ) as Pipeline;
+
+  return merged;
 }

test/config.test.ts
@@ -5,10 +5,12 @@ import { fakeProcess, getProjectFixturePath } from './fixtures';
 
 const fs = fsCb.promises;
 
-function* configSteps(configs: Config[]): IterableIterator<Step> {
-  for (const config of configs) {
-    yield* config.allSteps();
-  }
+function configSteps(configs: Config[]): Step[] {
+  return [...configs.flatMap((config) => config.allSteps())];
+}
+
+function configOuterSteps(configs: Config[]): Step[] {
+  return [...configs.flatMap((config) => config.outerSteps())];
 }
 
 async function configNames(scenario: string): Promise<string[]> {
@@ -44,12 +46,31 @@ describe('getConfig()', () => {
     ]);
   });
 
-  it('reads pipeline files and can iterate over steps - group', async () => {
-    const names = await configNames('group');
-    const steps = [...configSteps(await Config.getAll(getProjectFixturePath('group')))];
+  describe('allSteps()', () => {
+    it('reads pipeline files and can iterate over steps - group', async () => {
+      expect(await configNames('groups')).toStrictEqual(['foo1', 'foo2', 'foo3', 'foo4']);
+      expect(configSteps(await Config.getAll(getProjectFixturePath('groups'))).map((step) => step.key)).toStrictEqual([
+        'foo1',
+        'foo1-group',
+        'foo2',
+        'foo1-group', // Note: merging groups with the same key together hasn't happened at this point
+        'foo3',
+        'foo3-group',
+        'foo4',
+        'foo4-group',
+      ]);
+    });
+  });
 
-    // expect(names).toHaveLength(16);
-    expect(names).toStrictEqual(['foo1-group', 'foo3-group', 'foo1', 'foo2', 'foo3', 'foo4']);
+  describe('outerSteps()', () => {
+    it('reads pipeline files and can iterate over steps - group', async () => {
+      const names = await configNames('groups');
+      const steps = [...configOuterSteps(await Config.getAll(getProjectFixturePath('groups')))];
+      expect(names).toStrictEqual(['foo1', 'foo2', 'foo3', 'foo4']);
+
+      // Note: merging groups with the same key together hasn't happened at this point
+      expect(steps.map((step) => step.key)).toStrictEqual(['foo1-group', 'foo1-group', 'foo3-group', 'foo4-group']);
+    });
   });
 
   it('reads pipeline files and returns an array of config files - invalid', async () => {

commit b5b0b0d5df26909f313a3b55f500be42b0e74e75

Fix fallback early exit
Fixes bug introduced in c1b2cec62c7ea46a9525d0fd53a78822f72be575

src/commands/pipeline.ts
@@ -29,7 +29,6 @@ export default class Pipeline extends BaseCommand {
     } catch (err: unknown) {
       log('Failed to find base commit or diff changes, falling back to do a full build', err);
       Config.configureFallback(configs);
-      return Promise.resolve();
     }
 
     const yaml = dumpYaml(await mergePipelines(configs));